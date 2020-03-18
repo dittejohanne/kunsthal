0b6a56234d4671bc928816abe75c5ee82893b959
Johanne Justesen <johannejpoulsen@gmail.com>
2020-03-18 14:24:15 +0100

    koden

---
+++ js/script.js
@@ -0,0 +1,62 @@
+    function myFunction1() {
+        var checkBox = document.getElementById("myCheck");
+        var text = document.getElementById("boem");
+        if (checkBox.checked == true) {
+            boem.style.display = "block";
+        } else {
+            boem.style.display = "none";
+        }
+
+    }
+
+    function myTwo1() {
+        var checkBox = document.getElementById("two");
+        var text = document.getElementById("toeren");
+        if (checkBox.checked == true) {
+            toeren.style.display = "block";
+        } else {
+            toeren.style.display = "none";
+        }
+    }
+
+    function myThree1() {
+        var checkBox = document.getElementById("three");
+        var text = document.getElementById("kagedrengen");
+        if (checkBox.checked == true) {
+            kagedrengen.style.display = "block";
+        } else {
+            kagedrengen.style.display = "none";
+        }
+    }
+
+
+    function myFunction2() {
+        var checkBox = document.getElementById("myCheck3");
+        var text = document.getElementById("boem");
+        if (checkBox.checked == true) {
+            boem.style.display = "block";
+        } else {
+            boem.style.display = "none";
+        }
+
+    }
+
+    function myTwo2() {
+        var checkBox = document.getElementById("two3");
+        var text = document.getElementById("toeren");
+        if (checkBox.checked == true) {
+            toeren.style.display = "block";
+        } else {
+            toeren.style.display = "none";
+        }
+    }
+
+    function myThree2() {
+        var checkBox = document.getElementById("three3");
+        var text = document.getElementById("kagedrengen");
+        if (checkBox.checked == true) {
+            kagedrengen.style.display = "block";
+        } else {
+            kagedrengen.style.display = "none";
+        }
+    }
